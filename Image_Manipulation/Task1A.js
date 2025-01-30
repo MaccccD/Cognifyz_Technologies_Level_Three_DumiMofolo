1811528803512b4e11950d490ea23302fadaeaa6
Dumi Mofolo <Dumisanimofolo@gmail.com>
2025-01-30 19:03:02 +0200

    Nav Bar Fix Up Update

--- Image_Manipulation/Task1A.js
+++ Image_Manipulation/Task1A.js
@@ -1,6 +1,6 @@
 function openModal(imgElement){
     document.getElementById("imageModal").style.display = "block";
-    document.getElementById("modalImg").src = imgElement.src;
+    document.getElementById("modalImg").src = imgElement.src;// shows the actual image as per the src path on the img
 }
 function closeModal (){
     document.getElementById("imageModal").style.display = "none"; // close the X btn
